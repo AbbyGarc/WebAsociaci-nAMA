881d7a581d305174ab20a30f26653bfd107a31e7
AndreiAchim-1DAW <andrei.achim@educa.madrid.org>
2024-02-12 19:20:25 +0100

    Cambios en la página de registro y css

--- codigo/registro.js
+++ codigo/registro.js
@@ -2,7 +2,7 @@ function confirmPrivacidad (){
     var checkbox = document.getElementById('privacidad');
     checkbox.addEventListener( 'change', function() {
     if(this.checked) {
-       alert("Al marcar esta casillººººººa confirma que se ha leido las politicas de privacidad y está de acuerdo a ellas.");
+       alert("Al marcar esta casilla confirma que se ha leido las politicas de privacidad y está de acuerdo a ellas.");
     }
 });
 }
